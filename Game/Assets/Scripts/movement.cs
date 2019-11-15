e372ac0960237cfee5bed4b0689c0fbf0406baec
Anders Schultz Christiansen <aschristiansen@gmail.com>
2019-11-16 00:54:03 +0100

    Set up scene, main balls, and standard assets.

---
+++ Game/Assets/Scripts/movement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float thrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roll();
+    }
+
+    private void roll()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * thrust);
+        }
+    }
+}
