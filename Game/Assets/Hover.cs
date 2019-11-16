191e84f31855f46c73b6bca65d0b317d47d27352
Anders Schultz Christiansen <aschristiansen@gmail.com>
2019-11-16 14:26:55 +0100

    Added power ups that spawn randomly, hover, and rotate

---
+++ Game/Assets/Hover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public float hoverSpeed;
+    public float hoverHeight;
+
+    private Vector3 initialPosition;
+    private float yRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+        yRotate = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = initialPosition + new Vector3(0, Mathf.Cos(Time.time * hoverSpeed) * 0.5f, 0);
+        transform.Rotate(0, yRotate, 0);
+    }
+}
---
+++ Game/Assets/SpawnManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] powerUps;
+
+    public float minTime;
+    public float maxTime;
+    public GameObject floor;
+
+    private bool spawning;
+    private float currentTime;
+    private int currentPowerUp;
+    private float floorX;
+    private float floorZ;
+    private float spawnX;
+    private float spawnZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("spawnPowerUp");
+        floorX = floor.transform.lossyScale.x / 2;
+        floorZ = floor.transform.lossyScale.z / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnPowerUp()
+    {
+        while (true)
+        {
+            currentTime = Random.Range(minTime, maxTime); //Get a random time to spawn power ups
+            currentPowerUp = (int)Random.Range(0, powerUps.Length-1); //Get a random index of the powerup array
+
+            //The positions of the new power up (dependant on floor size)
+            spawnX = Random.Range(-floorZ, floorX);
+            spawnZ = Random.Range(-floorZ, floorZ);
+
+            //Instantiate a power up within the size of the arena
+            Instantiate(powerUps[currentPowerUp], new Vector3(spawnX, 1, spawnZ), Quaternion.identity);
+
+            print("X: " + spawnX + ", Z: " + spawnZ);
+            //print("Spawning!");
+            //print("Power Up: " + currentPowerUp);
+            //print("Curren Time: " + currentTime); //For debugging to see the current interval
+            yield return new WaitForSeconds(currentTime);
+        }
+
+    }
+}
