6b9f539a2e5f70495cd4990b9a334d5f32fb4640
Anders Schultz Christiansen <aschristiansen@gmail.com>
2019-11-16 18:53:47 +0100

    Added green and red pick up effect

--- Game/Assets/pickUp.cs
+++ Game/Assets/pickUp.cs
@@ -4,21 +4,66 @@ using UnityEngine;
 
 public class pickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float greenDuration;
+    public GameObject pickUpEffect;
+    public Material green;
+    public Material defaultPlayer;
 
     private void OnTriggerEnter(Collider other)
     {
         print("Pickup");
+        if(other.CompareTag("Player"))
+        {
+            if(gameObject.CompareTag("green"))
+            {
+                Debug.Log("Green picked up !");
+                StartCoroutine(PickUpGreen(other));
+            }
+
+            if (gameObject.CompareTag("red"))
+            {
+                Debug.Log("Red picked up !");
+                StartCoroutine(PickUpRed(other));
+            }
+        }
+    }
+
+    IEnumerator PickUpGreen(Collider player) // Green = You move faster
+    {
+
+        movement playermovement = player.GetComponent<movement>();
+
+        // When the effect kicks in
+        playermovement.thrust *= 2;
+        GameObject effect = Instantiate(pickUpEffect, player.transform.position, Quaternion.identity);
+        effect.transform.SetParent(player.transform);
+
+        player.GetComponent<Renderer>().material = green;
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        yield return new WaitForSeconds(greenDuration);
+
+        // When the effect ends
+        playermovement.thrust /= 2;
+        Destroy(effect);
+        player.GetComponent<Renderer>().material = defaultPlayer;
+
         Destroy(gameObject);
+    }
+
+    IEnumerator PickUpRed(Collider player) // Red = You get bigger
+    {
+        // When the effect kicks in
+        player.transform.localScale *= 3;
+
+        Destroy(gameObject);
+
+        yield return new WaitForSeconds(greenDuration);
+
+        // When the effect ends
+        player.transform.localScale /= 3;
+
     }
 }
